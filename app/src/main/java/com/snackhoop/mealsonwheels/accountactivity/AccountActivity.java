570858aaa49ca5f804242d20675b09923336d83a
malavancs <mlvnhari@gmail.com>
2018-03-08 12:36:46 +0530

    Malavan

--- app/src/main/java/com/snackhoop/mealsonwheels/accountactivity/AccountActivity.java
+++ app/src/main/java/com/snackhoop/mealsonwheels/accountactivity/AccountActivity.java
@@ -1,13 +1,15 @@
-package com.snackhoop.mealsonwheels;
+package com.snackhoop.mealsonwheels.accountactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.snackhoop.mealsonwheels.R;
+
+public class AccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_account);
     }
 }
